c665c3eb988b0fc8579ff273e552d2d4823b8289
Ania <anna.lesniak@copper.io>
2014-07-30 21:14:29 +0200

    [#1457604] Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reviewer::Application.config.secret_token = '31fe9d76f1192b74b9436f255d5bc15f4eb3d591c11b298c39e7d398125687f4b2ab0269bbaf7a600ec774fbfc2b69a33bcc2196105e2ab41f835f7713d2190f'
